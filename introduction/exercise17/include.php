3e9a0e4e30b61ebb853044d4490a67900532abb7
Fael123Programming <rafaelfonseca1020@gmail.com>
2022-06-06 20:16:14 -0300

    new files

--- introduction/exercise17/include.php
+++ introduction/exercise17/include.php
@@ -5,6 +5,12 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Using include HTML</title>
+    <style>
+        p {
+            text-align: justify;
+            font-family: Helvetica;
+        }
+    </style>
 </head>
 <body>
     <?php include 'header.html'?>
---
+++ introduction/exercise20/index.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Getting the Current Date and The Concatenation Operator</title>
+</head>
+<body>
+    <h1>PHP concatenation operator is a dot.</h1>
+    <?php echo 'Today is '. date('l').'.'?> Here's the latest news.
+    <br>
+    <script type="text/javascript">
+        document.write('Today is ' + Date());
+    </script>
+</body>
+</html>
